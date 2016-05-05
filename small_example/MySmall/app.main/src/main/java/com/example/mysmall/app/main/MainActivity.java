814bb181896f28e1dba20450a65565893378fa78
wangyang06 <wangyang06@xywy.com>
2016-05-05 15:34:41 +0800

    Revert "Test functions"
    This reverts commit 7e4cae3f3fea8f472c9a97c013d7feb247aaa416.

--- small_example/MySmall/app.main/src/main/java/com/example/mysmall/app/main/MainActivity.java
+++ small_example/MySmall/app.main/src/main/java/com/example/mysmall/app/main/MainActivity.java
@@ -2,10 +2,6 @@ package com.example.mysmall.app.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-
-import com.example.mylib.MyLib;
-import com.example.mylib2.MyLib2;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,8 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        MyLib.test();
-        Log.d("locald", "mylib2 test " + MyLib2.test());
     }
 }
